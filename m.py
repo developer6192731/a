9348818093ebd991fb623166efe0c66ff9aea021
developer6192731 <99138666+developer6192731@users.noreply.github.com>
2022-02-06 23:54:40 +0600

    Update m.py

--- m.py
+++ m.py
@@ -2,7 +2,18 @@ from tkinter import *
 
 r = Tk()
 
-a = Button(r, text="Hello")
+def cb():
+  q = t.get(1.0, "end-1c")
+  ans = int(q)**3
+  l.config(name=ans)
+  
+
+
+a = Button(r, text="Cube", command=cb)
+t = Text(r)
+l = Label(r)
+t.pack()
 a.pack()
+l.pack()
 
 r.mainloop()
